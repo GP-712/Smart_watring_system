9049eea383d07d6fb41321c20b03e0ebc8c7c903
saleh alkhamees <101878895+saleh-kh@users.noreply.github.com>
2022-12-25 14:49:47 +0300

    Added some comments and the data file used .

--- Machinlearning/cluster.py
+++ Machinlearning/cluster.py
@@ -4,39 +4,42 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score, calinski_harabasz_score
 import matplotlib.pyplot as plt
 
+# load the dataet
+df = pd.read_csv("../data1.csv")
 
-df = pd.read_csv("C:/Users/zerox/Downloads/data1.csv")
-
-df.info()
+# drop any rows with empty or missing values 
 df = df.dropna() 
 
-
+# split the data 80% for training and 20% for testing.
 train = df.iloc[:int(len(df)*0.8)]  
 test = df.iloc[int(len(df)*0.8):] 
 
-
+# crreate a scaler object 
 scaler = StandardScaler()
+
+# scale the data 
 train_scaled = scaler.fit_transform(train[['temp', 'humid', 'soil', 'soil_temp', 'salt']])
 test_scaled = scaler.transform(test[['temp', 'humid', 'soil', 'soil_temp', 'salt']])
 
-
+#training the model
 kmeans = KMeans(n_clusters=2)
 kmeans.fit(train_scaled)
 
-
+#check the model performance 
 silhouette_score = silhouette_score(test_scaled, kmeans.predict(test_scaled))
 calinski_harabasz_score = calinski_harabasz_score(test_scaled, kmeans.predict(test_scaled))
 
 print("Silhouette score:", silhouette_score)
 print("Calinski-Harabasz score:", calinski_harabasz_score)
 
-
+# get labels for the test data 
 predictions = kmeans.predict(test_scaled)
-
+# scatter plot 
 plt.scatter(test['soil'], test['temp'], c=predictions, cmap='viridis')
 plt.xlabel('Soil Moisture')
 plt.ylabel('temp')
 plt.title('K-means Clustering')
+# show the scatter plot 
 plt.show()
 
 
